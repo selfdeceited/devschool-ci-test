d1db7fa02f84799f73c0e482fc591579dd64392a
aryzhikov <aryzhikov@dodopizza.com>
2024-09-11 08:58:30 +0400

    show env variable usages

--- Program.cs
+++ Program.cs
@@ -21,6 +21,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/", () => $"Hello from {Environment.GetEnvironmentVariable("Env") ?? "local machine"}");
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
