06540aa645df4894215e57d2674f57bffe2d8cf2
aryzhikov <a.ryzhikov@dodopizza.com>
2024-10-28 16:22:15 +0300

    add unit tests

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace devschool_ci_unittests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
